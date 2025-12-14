efb2ec3d822f058a50d7dd053a314450ca98d256
Huy-DNA <huydo862003@gmail.com>
2025-12-14 15:12:56 +0700

    feat: add evaluation relation helpers

--- untyped_arithmetic_expressions/main.ml
+++ untyped_arithmetic_expressions/main.ml
@@ -6,3 +6,14 @@ type term =
   | TmSucc of info * term
   | TmPred of info * term
   | TmIsZero of info * term
+
+let rec isnumericval t = match t with
+    TmZero(_) -> true
+  | TmSucc(_, t1) -> isnumericval t1
+  | _ -> false
+
+let rec isval t = match t with
+  | TmTrue(_) -> true
+  | TmFalse(_) -> true
+  | t when isnumericval t -> true
+  | _ -> false

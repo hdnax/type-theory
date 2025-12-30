b4ced0ded952cc59a50dea06dbd48db868ea8376
Huy-DNA <huydo862003@gmail.com>
2025-12-30 07:42:23 +0700

    feat: extend the basic lambda evaluator with if and +

--- plai/lambda-calc-evaluator/README.md
+++ plai/lambda-calc-evaluator/README.md
@@ -8,3 +8,4 @@ Build a simple evaluator for an untyped lambda calculus extended with numbers an
 
 - [x] Implement an AST for variables, λ, application, booleans, numbers.
 - [x] Implement a basic evaluator (substitution-based is fine).
+- [x] Extend the interpreter with `if` and numeric `+`.
--- plai/lambda-calc-evaluator/main.rkt
+++ plai/lambda-calc-evaluator/main.rkt
@@ -6,12 +6,25 @@
   [varE (id : string)]
   [absE (param : string) (value : Exp)]
   [appE (abs : Exp) (arg : Exp)]
+  [ifE (condE : Exp) (thenE : Exp) (elseE : Exp)]
+  [plusE (left : Exp) (right : Exp)]
   [letE (id : string) (value : Exp) (exp : Exp)])
 
 (define-type Value
   [numV (n : number)]
   [boolV (b : boolean)]
   [funcV (param : string) (value : Exp) (env : (hashof string Value))])
+
+(define (isTruthy [v : Value]) : boolean
+  (type-case Value v
+    [numV (n) #t]
+    [boolV (b) b]
+    [funcV (param value env) #t]))
+
+(define (Value->number [v : Value]) : number
+  (type-case Value v
+    [numV (n) n]
+    [else (error 'Value->number "Expected a number")]))
 
 (define (calc [e : Exp] [env : (hashof string Value)]) : Value
   (type-case Exp e
@@ -30,6 +43,13 @@
               [funcV (param value env)
                      (calc value (hash-set env param argV))]
               [else (error 'app "The callee is not a function")]))]
+    [ifE (condE thenE elseE)
+         (let ([condV (calc condE env)])
+           (if (isTruthy condV) (calc thenE env) (calc elseE env)))]
+    [plusE (left right)
+           (let ([leftV (Value->number (calc left env))]
+                 [rightV (Value->number (calc right env))])
+             (numV (+ leftV rightV)))]
     [letE (id value exp)
           (calc exp (hash-set env id (calc value env)))]))
 

a0903ded512bafd62810b2e90b64c4146d633791
Huy-DNA <huydo862003@gmail.com>
2026-01-03 21:44:40 +0700

    feat: add all_zero computation

--- sf/lf/ch01-basics/tuples.v
+++ sf/lf/ch01-basics/tuples.v
@@ -22,4 +22,9 @@ Module TuplePlayground.
     | (bits B0 B0 B0 B0) => true
     | (bits _ _ _ _) => false
     end.
+
+  Compute (all_zero (bits B1 B0 B1 B0)).
+  (* ==> false : bool *)
+  Compute (all_zero (bits B0 B0 B0 B0)).
+  (* ==> true : bool *)
 End TuplePlayground.

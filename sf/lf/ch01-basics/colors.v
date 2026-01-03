016b30bad12b3a7354f808c7d8e7d2cd048c7860
Huy-DNA <huydo862003@gmail.com>
2026-01-03 20:57:25 +0700

    feat: add deep pattern matching in rocq

--- sf/lf/ch01-basics/colors.v
+++ sf/lf/ch01-basics/colors.v
@@ -14,12 +14,21 @@ Inductive rgb : Type :=
 Inductive color : Type :=
   | black
   | white
-  | primary (p : rgb).
+  | primary (p : rgb). (* Complex constructor *)
 
 (* Pattern matching for complex constructors *)
 Definition monochrome (c : color) : bool :=
   match c with
   | black => true
   | white => true
-  | primary p => false
+  | primary p => false (* Complex constructor pattern *)
+  end.
+
+(* Deep pattern matching *)
+Definition isred (c : color) : bool := 
+  match c with
+  | black => false
+  | white => false
+  | primary red => true (* Deep constructor pattern *)
+  | primary _ => false (* Wildcard pattern to match all values *)
   end.

b6f189186d6591e77fcb0355e8875c386be0e39c
Huy-DNA <huydo862003@gmail.com>
2026-01-03 19:19:43 +0700

    feat: add booleans in rocq

---
+++ sf/lf/ch01-basics/booleans.v
@@ -0,0 +1,23 @@
+(* vim:ft=coq *)
+
+Inductive bool : Type :=
+  | true
+  | false.
+
+Definition negb (b : bool) : bool :=
+  match b with
+  | true => false
+  | false => true
+  end.
+
+Definition andb (b1 : bool) (b2 : bool) : bool :=
+  match b1 with
+  | true => b2
+  | false => false
+  end.
+
+Definition orb (b1 : bool) (b2 : bool) : bool :=
+  match b1 with
+  | true => true
+  | false => b2
+  end.

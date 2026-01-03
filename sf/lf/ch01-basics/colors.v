dc3f9abbdd043889c2d1c41222a365b1a3ed6d6c
Huy-DNA <huydo862003@gmail.com>
2026-01-03 20:53:38 +0700

    feat: add pattern matching for complex constructors

--- sf/lf/ch01-basics/colors.v
+++ sf/lf/ch01-basics/colors.v
@@ -1,5 +1,11 @@
 (* vim:ft=coq *)
 
+Inductive bool : Type :=
+  | true
+  | false.
+
+(* More complex inductive types - Constructors with arguments *)
+
 Inductive rgb : Type :=
   | red
   | green
@@ -9,3 +15,11 @@ Inductive color : Type :=
   | black
   | white
   | primary (p : rgb).
+
+(* Pattern matching for complex constructors *)
+Definition monochrome (c : color) : bool :=
+  match c with
+  | black => true
+  | white => true
+  | primary p => false
+  end.

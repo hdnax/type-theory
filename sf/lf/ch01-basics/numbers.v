f217e3e7fd34c01116aaa1df68eced3214069cca
Huy-DNA <huydo862003@gmail.com>
2026-01-04 10:16:19 +0700

    feat: add pred for nat in rocq

--- sf/lf/ch01-basics/numbers.v
+++ sf/lf/ch01-basics/numbers.v
@@ -4,3 +4,15 @@
 Inductive nat : Type :=
   | O             (* zero *)
   | S (n : nat).  (* successor/scratch *)
+
+(** At this point, nat and otherNat just use different markers, other than that, they have no differences **)
+Inductive otherNat : Type :=
+  | stop
+  | tick (foo : otherNat).
+
+(* Give an interpretation to nat to distinguish it from otherNat *)
+Definition pred (n : nat) : nat :=
+  match n with
+  | O => O
+  | S n' => n'
+  end.

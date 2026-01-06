651497d804f7d04941e4f62925240d606f8299ab
Huy-DNA <huydo862003@gmail.com>
2026-01-06 07:23:10 +0700

    feat: add plus_id_exercise theorem

---
+++ sf/lf/ch01-basics/exercises/plus_id_exercise.v
@@ -0,0 +1,11 @@
+(* vim:ft=coq *)
+
+Theorem plus_id_exercise : forall n m o : nat,
+  n = m -> m = o -> n + m = m + o.
+Proof.
+  intros n m o.
+  intros H1 H2.
+  rewrite -> H1.
+  rewrite -> H2.
+  reflexivity.
+Qed.

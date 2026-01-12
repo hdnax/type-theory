570d908548585fdea4d156e11be1a86e9b4adef2
Huy-DNA <huydo862003@gmail.com>
2026-01-12 21:02:05 +0700

    feat: add identity_fn_applied_twice

---
+++ sf/lf/ch01-basics/exercises/identity_fn_applied_twice.v
@@ -0,0 +1,14 @@
+(* vim:ft=coq *)
+
+Theorem identity_fn_applied_twice :
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = x) ->
+  forall (b : bool), f (f b) = b.
+Proof.
+  intros f.
+  intros H.
+  intros b.
+  rewrite -> H.
+  rewrite -> H.
+  reflexivity.
+Qed.
